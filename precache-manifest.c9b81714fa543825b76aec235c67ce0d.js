8a2f8e1d540e6381f9866dd197dd85f55c468318
AmineDz <amine2014max@gmail.com>
2020-08-10 19:33:47 +0100

    Updates

---
+++ precache-manifest.c9b81714fa543825b76aec235c67ce0d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46fccc40a753e994b7a4e4fa6aa74a78",
+    "url": "/dine/index.html"
+  },
+  {
+    "revision": "fd9a0d0ea1d0026e0b47",
+    "url": "/dine/static/css/main.a1ef4233.chunk.css"
+  },
+  {
+    "revision": "e9d31ef0982084e48a88",
+    "url": "/dine/static/js/2.c6c237ad.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/dine/static/js/2.c6c237ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd9a0d0ea1d0026e0b47",
+    "url": "/dine/static/js/main.1660440e.chunk.js"
+  },
+  {
+    "revision": "2e10415cc4c941e6d1cd",
+    "url": "/dine/static/js/runtime-main.9d646786.js"
+  },
+  {
+    "revision": "a424e9b5d4d1152d5b109131323e15c2",
+    "url": "/dine/static/media/beef-desktop-tablet@2x.a424e9b5.jpg"
+  },
+  {
+    "revision": "20a771578ef06b6d4f8dda8ca576baab",
+    "url": "/dine/static/media/chocolate-desktop-tablet@2x.20a77157.jpg"
+  },
+  {
+    "revision": "2dfa948e2d2120d2691f8c77f2f12d05",
+    "url": "/dine/static/media/enjoyable-place-desktop@2x.2dfa948e.jpg"
+  },
+  {
+    "revision": "36532372987f39ab653e3acd0f1d974a",
+    "url": "/dine/static/media/enjoyable-place-tablet@2x.36532372.jpg"
+  },
+  {
+    "revision": "dff4dca8b078e146049b0e015d3788d5",
+    "url": "/dine/static/media/family-gathering-desktop@2x.dff4dca8.jpg"
+  },
+  {
+    "revision": "c583b43fdb1ea1c8c7f61453645449c3",
+    "url": "/dine/static/media/hero-bg-desktop@2x.c583b43f.jpg"
+  },
+  {
+    "revision": "d69ea4f8353adc5effcc0ec0e2541aee",
+    "url": "/dine/static/media/hero-bg-desktop@2x.d69ea4f8.jpg"
+  },
+  {
+    "revision": "f8e1664f5798769f36dc57ce000cef41",
+    "url": "/dine/static/media/hero-bg-mobile@2x.f8e1664f.jpg"
+  },
+  {
+    "revision": "b5bbd83a5b30addfcc89c3cf49d26c6e",
+    "url": "/dine/static/media/hero-bg-tablet@2x.b5bbd83a.jpg"
+  },
+  {
+    "revision": "9ee235724df584d44cd16efcb0d0a454",
+    "url": "/dine/static/media/locally-sourced-desktop@2x.9ee23572.jpg"
+  },
+  {
+    "revision": "8c0135c080eca620327740f425215391",
+    "url": "/dine/static/media/locally-sourced-mobile@2x.8c0135c0.jpg"
+  },
+  {
+    "revision": "6b39764dd020d7e41c9068371c8c5cf8",
+    "url": "/dine/static/media/locally-sourced-tablet@2x.6b39764d.jpg"
+  },
+  {
+    "revision": "932ec5bc8d11397b3eca4d28efa9221b",
+    "url": "/dine/static/media/logo.932ec5bc.svg"
+  },
+  {
+    "revision": "2f7c5e37374f90b9a1da606a086410ab",
+    "url": "/dine/static/media/pattern-curve-bottom-right.2f7c5e37.svg"
+  },
+  {
+    "revision": "2465b8690b7e5ee4df724892e32be286",
+    "url": "/dine/static/media/pattern-curve-top-left.2465b869.svg"
+  },
+  {
+    "revision": "8e92e19517a7b2e6058a1644248e5b8e",
+    "url": "/dine/static/media/pattern-curve-top-right.8e92e195.svg"
+  },
+  {
+    "revision": "c2c518b398ec0dd6a19b3eb06925a683",
+    "url": "/dine/static/media/pattern-divide.c2c518b3.svg"
+  },
+  {
+    "revision": "77989ac9c8c298c7c7b164f724c3dcb9",
+    "url": "/dine/static/media/pattern-lines.77989ac9.svg"
+  },
+  {
+    "revision": "831149fd1ba542fbe0886d29777ff837",
+    "url": "/dine/static/media/salmon-desktop-tablet@2x.831149fd.jpg"
+  },
+  {
+    "revision": "9c157134af789f2116b26430be267e40",
+    "url": "/dine/static/media/social-events-desktop@2x.9c157134.jpg"
+  },
+  {
+    "revision": "3a427a80553bf94f63a0bc98d1b40a42",
+    "url": "/dine/static/media/special-events-desktop@2x.3a427a80.jpg"
+  }
+]);
